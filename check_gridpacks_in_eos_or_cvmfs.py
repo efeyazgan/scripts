8225a1c82641b72db35b52dbd0f6b4135d848ebb
Efe Yazgan <efe.yazgan@cern.ch>
2018-03-29 11:33:01 +0200

    Update check_gridpacks_in_eos_or_cvmfs.py

--- check_gridpacks_in_eos_or_cvmfs.py
+++ check_gridpacks_in_eos_or_cvmfs.py
@@ -10,11 +10,7 @@ requests = [
            ]
 path1 = '/cvmfs/cms.cern.ch/phys_generator'
 path2 = '/eos/cms/store/group/phys_generator/cvmf'
-print(path1)
-print(path2)
-##########################################
 ######## START LOOP OVER PREPIDS #########
-##########################################
 for prepid in requests:
 
         os.system('echo '+prepid)
@@ -24,17 +20,14 @@ for prepid in requests:
         os.system('wget -q https://cms-pdmv.cern.ch/mcm/public/restapi/requests/get_fragment/'+prepid+' -O '+prepid)
         gridpack_cvmfs_path = os.popen('grep \/cvmfs '+prepid+'| grep -v \'#args\' ').read()
         gridpack_cvmfs_path = gridpack_cvmfs_path.split('\'')[1]
-#       print type(gridpack_cvmfs_path)
         gridpack_eos_path = gridpack_cvmfs_path.replace("/cvmfs/cms.cern.ch/phys_generator","/eos/cms/store/group/phys_generator/cvmfs")
-#       print (gridpack_eos_path)       
-        print (gridpack_cvmfs_path)
-#       os.system('tar xf '+gridpack_eos_path+' -C'+my_path+'/'+prepid)
+#################### Select to Check EOS or CVMFS ####################################
+#        os.system('tar xf '+gridpack_eos_path+' -C'+my_path+'/'+prepid)
         os.system('tar xf '+gridpack_cvmfs_path+' -C'+my_path+'/'+prepid)
+######################################################################################
         os.system('more '+my_path+'/'+prepid+'/'+'runcmsgrid.sh | grep "FORCE IT TO"')
         os.system('grep _CONDOR_SCRATCH_DIR '+my_path+'/'+prepid+'/'+'mgbasedir/Template/LO/SubProcesses/refine.sh')
         os.system('grep "= ickkw" '+my_path+'/'+prepid+'/'+'process/madevent/Cards/run_card.dat')
         os.system('echo "------------------------------------"')
         os.system('rm '+prepid)
-##########################################
 ######## END LOOP OVER PREPIDS ###########
-##########################################

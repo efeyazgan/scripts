e4966578db861ea0b05bf2e0c58619d57690657b
Efe Yazgan <efe.yazgan@cern.ch>
2018-05-03 11:09:37 +0200

    Update check_gridpacks_in_eos_or_cvmfs.py

--- check_gridpacks_in_eos_or_cvmfs.py
+++ check_gridpacks_in_eos_or_cvmfs.py
@@ -1,20 +1,20 @@
 import os,sys
+
 my_path = '/tmp/'+os.environ['USER']+'/replace_gridpacks/'
 
+requests=[]
 requests = [
-'EXO-RunIISpring18wmLHEGS-00011',
-'EXO-RunIISpring18wmLHEGS-00012',
-'EXO-RunIISpring18wmLHEGS-00013',
-'EXO-RunIISpring18wmLHEGS-00014',
-'EXO-RunIISpring18wmLHEGS-00015',
-           ]
-path1 = '/cvmfs/cms.cern.ch/phys_generator'
-path2 = '/eos/cms/store/group/phys_generator/cvmf'
+#'HIG-RunIISpring18wmLHEGS-00006',
+]
+
+
+for num in range(0,3):
+       requests.append('HIG-RunIIFall17wmLHEGS-0'+str(num+1627))
+print requests
+
 ######## START LOOP OVER PREPIDS #########
 for prepid in requests:
-
         os.system('echo '+prepid)
-
         os.system('mkdir -p '+my_path+'/'+prepid)
         os.chdir(my_path+'/'+prepid)
         os.system('wget -q https://cms-pdmv.cern.ch/mcm/public/restapi/requests/get_fragment/'+prepid+' -O '+prepid)
@@ -22,11 +22,12 @@ for prepid in requests:
         gridpack_cvmfs_path = gridpack_cvmfs_path.split('\'')[1]
         gridpack_eos_path = gridpack_cvmfs_path.replace("/cvmfs/cms.cern.ch/phys_generator","/eos/cms/store/group/phys_generator/cvmfs")
 #################### Select to Check EOS or CVMFS ####################################
-#        os.system('tar xf '+gridpack_eos_path+' -C'+my_path+'/'+prepid)
+#       os.system('tar xf '+gridpack_eos_path+' -C'+my_path+'/'+prepid)
         os.system('tar xf '+gridpack_cvmfs_path+' -C'+my_path+'/'+prepid)
-######################################################################################
-        os.system('more '+my_path+'/'+prepid+'/'+'runcmsgrid.sh | grep "FORCE IT TO"')
-        os.system('grep _CONDOR_SCRATCH_DIR '+my_path+'/'+prepid+'/'+'mgbasedir/Template/LO/SubProcesses/refine.sh')
+#####################################################################################
+        os.system('more '+my_path+'/'+prepid+'/'+'runcmsgrid.sh | grep -c "FORCE IT TO"')
+        os.system('grep -c _CONDOR_SCRATCH_DIR '+my_path+'/'+prepid+'/'+'mgbasedir/Template/LO/SubProcesses/refine.sh')
+        os.system('grep -c _CONDOR_SCRATCH_DIR '+my_path+'/'+prepid+'/'+'process/madevent/SubProcesses/refine.sh')
         os.system('grep "= ickkw" '+my_path+'/'+prepid+'/'+'process/madevent/Cards/run_card.dat')
         os.system('echo "------------------------------------"')
         os.system('rm '+prepid)

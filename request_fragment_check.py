c2e12107afc518109bb11a7bdc182602a424089f
Efe Yazgan <efe.yazgan@cern.ch>
2018-08-31 13:56:03 +0200

    Update request_fragment_check.py

--- request_fragment_check.py
+++ request_fragment_check.py
@@ -14,7 +14,6 @@ for r in res:
     pi = r['prepid']
     dn = r['dataset_name']
     print ("'"+r['prepid']+"',",r['dataset_name'])
-    print (pi)
     os.system('wget -q https://cms-pdmv.cern.ch/mcm/public/restapi/requests/get_fragment/'+pi+' -O '+pi)
     if "powheg" in dn or "Powheg" in dn or "POWHEG" in dn :
         powheg_check = os.popen('grep -c pythia8PowhegEmissionVetoSettings '+pi).read()

83708362f14598d95f521c32ed280192a5e746ed
Efe Yazgan <efe.yazgan@cern.ch>
2018-09-04 09:16:01 +0200

    Update request_fragment_check.py

--- request_fragment_check.py
+++ request_fragment_check.py
@@ -3,12 +3,12 @@ os.system('source /afs/cern.ch/cms/PPD/PdmV/tools/McM/getCookie.sh')
 os.system('cern-get-sso-cookie -u https://cms-pdmv.cern.ch/mcm/ -o ~/private/prod-cookie.txt --krb --reprocess')
 sys.path.append('/afs/cern.ch/cms/PPD/PdmV/tools/McM/')
 
-from rest import *
+from rest import McM
 from json import dumps
 
-mcm = restful(dev=False)
+mcm = McM(dev=True)
 
-res = mcm.getA('requests', query='prepid=TOP-RunIISummer15wmLHEGS-00023')
+res = mcm.get('requests', query='prepid=TOP-RunIISummer15wmLHEGS-00023')
 
 for r in res:
     pi = r['prepid']
@@ -17,7 +17,7 @@ for r in res:
     os.system('wget -q https://cms-pdmv.cern.ch/mcm/public/restapi/requests/get_fragment/'+pi+' -O '+pi)
     if "powheg" in dn or "Powheg" in dn or "POWHEG" in dn :
         powheg_check = os.popen('grep -c pythia8PowhegEmissionVetoSettings '+pi).read()
-        print powheg_check
+        print "no known inconsistency in the fragment w.r.t. the name of the dataset"
         powheg_check = int(powheg_check)
         if powheg_check == 0 :
             print "Wrong fragment: powheg in dataset name but no pythia8PowhegEmissionVetoSettings in fragment"

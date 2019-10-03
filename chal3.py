82cdee0c3197478af2caa3bb33ec2374c2db436d
Efe Yazgan <efe.yazgan@cern.ch>
2019-10-03 20:16:26 +0200

    Update chal3.py

--- chal3.py
+++ chal3.py
@@ -38,43 +38,44 @@
 import time
 import random
 
+#define the function
 def addtwomethod1(A,tot):
-    A=sorted(A)
-    n = len(A)
-    L = 0
-    R = n-1
+    A=sorted(A) #sort the list A
+    n = len(A) #n = number of elements in A
+    L = 0 #index of the first element of A
+    R = n-1 #index of the last element of A
     a1 = []
     a2 = []
-    newlist = []
-    while L < R:
-        if (A[L]+A[R] == tot):
-            a1.append(A[L])
-            a2.append(A[R])
-            L=L+1
-            R=R-1
-        elif (A[L]+A[R] < tot):
+    while L < R: #scan the elements of A 
+        if (A[L]+A[R] == tot): # the condition for the two elements of the list adding up to the desired value (tot)
+            a1.append(A[L]) #add one element of the pair that meets the condition to the list a1
+            a2.append(A[R]) #add other element of the pair that meets the condition to the list a2
+            L=L+1 #to scan the next element
+            R=R-1 #to scan the next element
+        elif (A[L]+A[R] < tot): #when condition is not satistified like this increment L to scan the next element
             L=L+1  
-        elif (A[L]+A[R] > tot):
+        elif (A[L]+A[R] > tot): #when condition is not satistified like this decrement R to scan the next element
             R=R-1  
     print("# The pairs that add up to",tot,":")    
-    for x,y in zip(a1,a2):
+    for x,y in zip(a1,a2): # zip pairs the elements in a1 and a2 (this is just for conveniently printing) 
         print("# (",x,",",y,")")
     if len(a1) == 0:
         print("# None")    
                 
 def addtwomethod2(A,tot):
+    #create empty set
     hset = set()
-    for i in range(0, len(A)):
-        test = tot-A[i]
-        if test in hset:
-            print("# Pair is",A[i],test,"for sum:",tot)
+    for i in range(0, len(A)): # loop over elements of the list A
+        test = tot-A[i] # candidate number to be tested for existence in A
+        if test in hset: 
+            print("# Pair is",A[i],test,"for sum:",tot) # resulting pair is {A[i], sum-A[i]}
             return
-        hset.add(A[i])    
+        hset.add(A[i]) #add the corresponding element A[i] to hset    
     print("# No pairs that add up to",tot)          
                 
 #a few examples:
 thelist = [4,2,8,0,1,-2,9,13445,1,4,3,6,6];
-#thelist = random.sample(range(0, 1000000), 1000000)
+#thelist = random.sample(range(0, 1000000), 1000000) # <--- to get a much larger list quickly
 print("# Running method 1")
 print("# ---------------------")
 start1 = time.time()

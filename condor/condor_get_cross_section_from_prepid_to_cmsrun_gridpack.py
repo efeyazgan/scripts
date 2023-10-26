6177c270eaf5aa82fee94000d8808c9f7577d6da
Efe Yazgan <efe.yazgan@cern.ch>
2023-10-26 14:20:15 +0200

    Update condor_get_cross_section_from_prepid_to_cmsrun_gridpack.py

--- condor/condor_get_cross_section_from_prepid_to_cmsrun_gridpack.py
+++ condor/condor_get_cross_section_from_prepid_to_cmsrun_gridpack.py
@@ -9,18 +9,26 @@ parser = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter,
     description=textwrap.dedent(''' help '''))
 parser.add_argument('--file', type=str, help="gridpack filename", nargs='+')
+parser.add_argument('--interference', type=int, help="interference A0/S0 or not", nargs='+')
 args = parser.parse_args()
 
 filen = ""
 
 if args.file is not None:
     filen = args.file[0]
+if args.interference is not None:
+    is_interference = args.interference[0] 
+
+if is_interference == 1:
+    path = '/eos/cms/store/group/phys_generator/cvmfs/gridpacks/UL/13TeV/madgraph/V5_2.6.5/g2HDM/ttc_interference/'
+else:
+    path = '/eos/cms/store/group/phys_generator/cvmfs/gridpacks/UL/13TeV/madgraph/V5_2.6.5/g2HDM/ttc/'
+#mainfolder = "/afs/cern.ch/user/e/efe/workspace_afs/testmgpythiawithcmssw/CMSSW_10_6_9/src"
+mainfolder = "/afs/cern.ch/user/e/efe/workspace_afs/testmgpythiawithcmssw_12_4_8/CMSSW_12_4_8/src"
 
 
-path = '/eos/cms/store/group/phys_generator/cvmfs/gridpacks/UL/13TeV/madgraph/V5_2.6.5/g2HDM/ttc/'
-mainfolder = "/afs/cern.ch/user/e/efe/workspace_afs/testmgpythiawithcmssw/CMSSW_10_6_9/src"
-
-fname = filen.split('_slc7')[0].split('g2HDM_ttc_')[1]
+#fname = filen.split('_slc7')[0].split('g2HDM_ttc_')[1]
+fname = filen.split('_el8')[0].split('g2HDM_ttc_')[1]
 print(fname)
 os.popen('rm -rf '+fname).read()
 os.popen('mkdir '+fname).read()
@@ -30,18 +38,22 @@ fragment_file_tmp = fold +'/' + fname+'tmp_cfg.py'
 fragment_file = fold +'/' + fname+'_cfg.py'
 os.chdir(fold)
 print(os.getcwd())
-print(fragment_file_tmp)
+#print(fragment_file_tmp)
 if os.path.isfile(fragment_file_tmp):
     f1 = open(fragment_file_tmp,"r+")
     f2 = open(fragment_file,"w")
     data_f1 = f1.read()
-    data_f2 = data_f1.replace("g2HDM_ttc_a0_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz",filen)
+    data_f2 = data_f1.replace("g2HDM_ttc_a0_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz",filen)  #g2HDM_ttc_a0_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz
+    data_f2 = data_f2.replace("/cvmfs/cms.cern.ch/phys_generator/","/eos/cms/store/group/phys_generator/cvmfs/") 
+    print(data_f2)
+    if is_interference == 1:
+        data_f2 = data_f2.replace("g2HDM/ttc","g2HDM/ttc_interference")
     f1.close()
     f2.write(data_f2)    
     f2.close()
     os.popen('rm '+fragment_file_tmp).read()
     log_file = fold +'/' + fname+'_log.txt'
-    os.popen('scram b -j8').read()
+#    os.popen('scram b -j8').read()
     cmd = 'cmsRun -e -j test.xml '+fragment_file
     print(cmd)
     os.popen(cmd+' >& '+log_file).read()
